e11245ae2caaf5192f9656768e3eb7c828ed1580
lyn7568 <1271778060@qq.com>
2020-03-04 15:51:20 +0800

    上传

---
+++ src/components/meeting-box/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-col :span="4" style="margin-left:30px">
+    <el-card :body-style="{ padding: '8px' }">
+      <div class="bg" :style="{backgroundImage: 'url(' + httpimage + ')', backgroundSize:'cover'}"></div>
+      <div style="padding: 14px;">
+        <span style="font-size:20px">{{this.list.nameCn}}</span>
+        <div class="bottom clearfix">
+          <time class="time">{{this.list.address}}</time>
+          <p class="time">举办时间:{{this.list.startTime}}</p>
+          <!-- <el-button type="text" class="button">操作按钮</el-button> -->
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      httpimage:
+        "http://a2.att.hudong.com/36/48/19300001357258133412489354717.jpg"
+    };
+  },
+  props: {
+    list: Object
+  },
+  components: {},
+  computed: {},
+  beforeMount() {},
+  mounted() {},
+  methods: {},
+  watch: {}
+};
+</script>
+<style scoped lang='scss'>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+.bg {
+  width: 100%;
+  height: 250px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>
